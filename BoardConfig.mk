5c9483283d9abd93d331df248ec76cd8151e9d63
councilcj <118770603+councilcj@users.noreply.github.com>
2026-01-30 14:13:53 -0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,31 +29,31 @@ TARGET_SCREEN_DENSITY := 280
 
 # For MediaTek Kernel 5.15+, we often need these to let the DTB handle the display
 RECOVERY_GRAPHICS_FORCE_USE_LINUX_FB := true
-TW_NO_UI := false # Set to false if you want to try the GUI now that we have a DTB
+TW_NO_UI := false 
 TW_RECOVERY_BUTTONS := true
 
 # --- BUTTONS & INPUT ---
 BOARD_HAS_NO_SELECT_BUTTON := false
 TW_INPUT_BLACKLIST := "hbtp_vm,fts_ts"
 
-# Kernel Configuration - HEADER V4 (No manual offsets needed)
+# Kernel Configuration - HEADER V4
 BOARD_BOOTIMG_HEADER_VERSION := 4
-BOARD_KERNEL_PAGESIZE := 4096 # Most MT6835 use 4KB pages
+BOARD_KERNEL_PAGESIZE := 4096 
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 
-# --- THE "PREBUILT DNA" (The Flicker Fixers) ---
+# --- THE "PREBUILT DNA" ---
 TARGET_FORCE_PREBUILT_KERNEL := true
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/kernel
-BOARD_PREBUILT_DTBIMAGE := $(DEVICE_PATH)/dtb.img
-BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 
-# Adding DTBO - This is critical for the MT6835 display driver
+# DTB Settings
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_PREBUILT_DTBIMAGE := $(DEVICE_PATH)/dtb.img
+
+# DTBO Settings - REMOVED CUSTOM_DTBOIMG TO FIX OVERRIDE ERROR
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/dtbo.img
-BOARD_CUSTOM_DTBOIMG := $(DEVICE_PATH)/dtbo.img
 
 # --- THE "NO-PANIC" COMMAND LINE ---
-# We keep these clean to let the DTB do the heavy lifting
 BOARD_KERNEL_CMDLINE := androidboot.boot_devices=soc/11270000.ufshci
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=fastboot
 BOARD_KERNEL_CMDLINE += loop.max_part=7
@@ -62,7 +62,7 @@ BOARD_KERNEL_CMDLINE += video=720x1604@60
 
 # Platform & Partitions
 TARGET_BOARD_PLATFORM := mt6835
-BOARD_FLASH_BLOCK_SIZE := 131072 # Standard for UFS
+BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
 TARGET_COPY_OUT_VENDOR := vendor
 
