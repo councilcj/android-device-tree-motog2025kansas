29793f56066ebc866bb58dcc9f5e3819e3e67531
councilcj <118770603+councilcj@users.noreply.github.com>
2026-01-30 14:31:04 -0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -2,17 +2,12 @@
 PRODUCT_NAME := kansas
 PRODUCT_DEVICE := kansas
 PRODUCT_BRAND := motorola
-PRODUCT_MODEL := moto g play 5G - 2024
-
-# --- THE NINJA FIX ---
-# Removed dtb.img and dtbo.img from here because BoardConfig.mk 
-# handles them via BOARD_PREBUILT_DTBIMAGE and BOARD_PREBUILT_DTBOIMAGE.
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/kernel:kernel
+PRODUCT_MODEL := moto g 5G - 2025
 
 # Recovery / FSTAB
+# We keep this because the build system doesn't auto-copy fstab
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/recovery.fstab:recovery/root/system/etc/recovery.fstab
+$(DEVICE_PATH)/recovery.fstab:recovery/root/system/etc/recovery.fstab
 
 # Inherit from common AOSP/TWRP parts
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_no_telephony.mk)
